1e3b95355dc1d47a5b1b968e2ec0a69491ef46b7
yuminglong318 <yuminglong318@gmail.com>
2023-12-20 07:13:11 +0000

    fix: issues

--- download_pdf.py
+++ download_pdf.py
@@ -1,8 +1,11 @@
 import requests
 import json
 import os
+import re
 
 def download_pdf(title, url, dir):
+
+    title = re.sub(r'[<>:"/\\|?*]', '', title)
     filename = os.path.join(dir, title)
 
     response = requests.get(url, stream=True)
@@ -33,14 +36,53 @@ if __name__ == '__main__':
             print(year)
             for pdf in pdfs:
                 
-                progress = pdfs.index(pdf)
+                progress = pdfs.index(pdf) + 1
                 total = len(pdfs)
 
                 bar_width = 100
                 filled_length = int(bar_width * progress / total)
                 bar = '█' * filled_length + '-' * (bar_width - filled_length)
 
-                download_pdf(pdf.get('title'), pdf.get('pdf_link'), dest_dir)
                 print(f"\r Downloading {progress}/{total}: [{bar}]", end= '', flush= True)
+
+                try:
+                    download_pdf(pdf.get('title'), pdf.get('pdf_link'), dest_dir)
+                except Exception as e:
+                    print(pdf.get('title'))
+
+            print("\n")
+
+    # Book Downloading
+    print('Downloading Books')
+    for dir in os.listdir('./book_list'):
+        print(f"{dir}")
+        subdir = os.path.join('./book_list', dir)
+        for file in os.listdir(subdir):
+            filepath = os.path.join(subdir, file)
+            year = file.split('.')[0][-4:]
+            dest_dir = os.path.join('./Books', dir, year)
+            if not os.path.exists(dest_dir):
+                os.makedirs(dest_dir)
+            
+            with open(filepath, "r", encoding= "utf-8") as f:
+                pdfs = json.load(f)
+            
+            print(year)
+            for pdf in pdfs:
+                
+                progress = pdfs.index(pdf) + 1
+                total = len(pdfs)
+
+                bar_width = 100
+                filled_length = int(bar_width * progress / total)
+                bar = '█' * filled_length + '-' * (bar_width - filled_length)
+
+                print(f"\r Downloading {progress}/{total}: [{bar}]", end= '', flush= True)
+
+                try:
+                    download_pdf(pdf.get('title'), pdf.get('pdf_link'), dest_dir)
+                except Exception as e:
+                    print(pdf.get('title'))
+                
 
             print("\n")
